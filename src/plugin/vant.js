f999650428093f7076fed0a0ad3e4e0c3415140e
鱼多多 <1049218799@qq.com>
2019-11-16 16:53:46 +0800

    主分支优化

--- src/plugin/vant.js
+++ src/plugin/vant.js
@@ -1,3 +1,18 @@
 import Vue from 'vue';
-import { Button } from 'vant';
+import {
+  Button,
+  NavBar,
+  Tabbar,
+  TabbarItem,
+  Icon,
+  Toast ,
+  List 
+} from 'vant';
 Vue.use(Button);
+Vue.use(NavBar);
+Vue.use(Tabbar);
+Vue.use(TabbarItem);
+Vue.use(Icon);
+Vue.use(List);
+
+Vue.prototype.$Toast=Toast
