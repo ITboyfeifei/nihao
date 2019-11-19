89f8a0caab20aebf25fa84c7911533538e68f171
鱼多多 <1049218799@qq.com>
2019-11-19 08:59:06 +0800

    商品详情修正

--- src/components/goods/GoodsIntroduce.vue
+++ src/components/goods/GoodsIntroduce.vue
@@ -1,17 +1,49 @@
 <template>
-    <div class="mainBox">
-        111
+    <div class="mainBox2">
+        <div class="main" v-for="(item, index) in list" :key="index">
+            <h4>{{item.title}}</h4>
+            <hr>
+            <div v-html="item.content"></div>
+        </div>
     </div>
 </template>
 <script>
 export default {
-    
+    data(){
+        return{
+            id:this.$route.params.id,
+            list:[]
+        }
+    },
+    created() {
+        this.getGoodsIntroduce()
+       
+        
+    },
+    methods: {
+        async getGoodsIntroduce(){
+            const data =await this.$http.get(`/api/goods/getdesc/${this.id}`)
+            if(data.status!=200)return this.$Toast.fail("获取失败")
+            this.list=data.data.message
+            console.log(this.list);
+
+
+            
+        }
+    },
 }
 </script>
 <style lang="less" scoped>
-    .mainBox{
+    .mainBox2{
+        overflow: hidden;
          margin-top: 40px;
          margin-bottom: 50px;
-         background-color: pink;
+        //  background-color: pink;
+    }
+    .main{
+        h4{
+            text-align: center;
+            color: skyblue;
+        }
     }
 </style>
